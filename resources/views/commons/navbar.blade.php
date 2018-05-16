a92ade081a930b1aed8ea8b45605f9dded94d622
foundnation12 <foundnation12@ez.web.ne.jp>
2018-05-16 13:37:32 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,9 +12,12 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', 'タスクの投稿') !!}</li>
-                    <li>{!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}</li>
-                    <li><a href="#">Login</a></li>
+                    @if (Auth::check())
+                        <li>{!! link_to_route('tasks.create', 'タスクの投稿') !!}</li>
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login.get', 'Login') !!}</li>
+                    @endif
                 </ul>
             </div>
         </div>
