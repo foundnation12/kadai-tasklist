a53978197d685d5d141c8595ca544f09894e9be2
foundnation12 <foundnation12@ez.web.ne.jp>
2018-05-17 09:13:01 +0000

    add user_id migration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,30 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        <h1>タスクリスト一覧</h1>
 
-    <h1>タスクリスト一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>ステータス</th>
-                    <th>メッセージ</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                    <tr>
-                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->status }}</td>
-                        <td>{{ $task->content }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-    
-    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
-
+            @if (count($tasks) > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>ステータス</th>
+                            <th>メッセージ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                                <td>{{ $task->status }}</td>
+                                <td>{{ $task->content }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
+            
+            {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
+            
+    @else
+        <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the TaskList</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
 @endsection
